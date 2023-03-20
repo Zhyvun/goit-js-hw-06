4353c44f0293deb6f56b503d1fd6355b475224ca
Zhyvun Volodymyr <zhionik@gmail.com>
2023-03-21 01:53:21 +0200

    Finished Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -12,29 +12,82 @@
 
 Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.    
 */
+
+//! Поки складно
+
+//TODO:
+/*
+1. Виводим посилання на необхідні об'єкти (<div>).
+2. Прописати початкові стилі <div>
+3. Створи функцію createBoxes(amount)
+4. Створити функцію очистки destroyBoxes()
+
+*/
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-//! Поки складно
-
-//TODO:
-/*
-1. Виводим посилання на об'єкти.
-2. Мабуть треба прописати стилі <div>
-
-Створи функцію createBoxes(amount)
-Створити функцію очистки destroyBoxes()
-
-
-
-*/
+//1.
 const controls = document.getElementById("controls");
-const inputNumberEl = document.querySelector('[type="number"]');
+const insNumEl = document.querySelector('[type="number"]');
 const createBtnEl = document.querySelector("button[data-create]");
 const destroyBtnEl = document.querySelector("button[data-destroy]");
-const divBoxesEl = document.getElementById("boxes");
+const divEl = document.getElementById("boxes");
 
 // console.log(controls);
+
+//2.
+divEl.style.display = "flex";
+divEl.style.flexWrap = "wrap";
+divEl.style.alignItems = "center";
+divEl.style.marginTop = "30px";
+
+// Вішаєм слухача на кнопку createBtnEl
+createBtnEl.addEventListener("click", () => {
+  // console.log(insNumEl.value);
+
+  // Прописуємо умову на корректність введених чисел (перевіряємо зі значеннями min="1" max="100" )
+  const insert = insNumEl.value.trim();
+  if (
+    Number(insert) > Number(insNumEl.max) ||
+    Number(insert) < Number(insNumEl.min)
+  ) {
+    alert(
+      "Людино Добра!🙏 Не балуйся, введи числа від 1 до 100 ... бо не лізе 👀"
+    );
+  } else {
+    createBoxes(insert);
+  }
+  insNumEl.value = "";
+});
+
+//3.
+function createBoxes(quantity) {
+  let size = 30;
+  const boxesArr = [];
+  for (let i = 0; i < quantity; i += 1) {
+    size += 10 * i;
+    const div = `<div 
+    class="item" style="display: block; 
+    margin-right: 10px;
+    margin-bottom: 30px;
+    background-color: ${getRandomHexColor()}; 
+    width: ${size}px; height: ${size}px;">
+    </div>`;
+    boxesArr.push(div);
+  }
+  divEl.insertAdjacentHTML("beforeend", boxesArr.join(""));
+  // console.log("arr length", boxesArr.length);
+}
+
+// Вішаєм слухача на кнопку destroyBtnEl
+destroyBtnEl.addEventListener("click", destroyBoxes);
+
+//4.
+function destroyBoxes() {
+  insNumEl.value = "";
+  divEl.innerHTML = "";
+}
